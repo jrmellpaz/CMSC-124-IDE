31bd222f48af75c9b76ec89337dce4e52b4c71ae
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2024-10-19 22:17:02 +0800

    Fixed scrollbar design when there is overflow of text & added the number of lines functionality.

--- script.js
+++ script.js
@@ -17,3 +17,44 @@ textarea.addEventListener('keydown', e => {
         insertTabCharacter();
     }
 });
+
+// Counting the number of lines in the textarea
+const linesChip = document.querySelector('[lines-chip]');
+const linesChipSymbol = document.querySelector('[lines-chip-symbol]');
+["mouseup", "keyup"].forEach(e => textarea.addEventListener(e, () => {
+    let style = (window.getComputedStyle) ? window.getComputedStyle(textarea) : textarea.currentStyle;
+    let taLineHeight = parseInt(style.lineHeight, 10);
+    let calculateContentHeight = (taLineHeight => {
+        let origHeight = textarea.style.height;
+        let height = textarea.offsetHeight;
+        let scrollHeight = textarea.scrollHeight;
+        let overflow = textarea.style.overflow;
+
+        if(height >= scrollHeight) {
+            textarea.style.height = (height + taLineHeight) + "px";
+            textarea.style.overflow = "hidden";
+
+            if(scrollHeight < textarea.scrollHeight) {
+                while(textarea.offsetHeight >= textarea.scrollHeight) {
+                    textarea.style.height = (height -= taLineHeight) + "px";
+                }
+
+                while(textarea.offsetHeight < textarea.scrollHeight) {
+                    textarea.style.height = (height++) + "px";
+                }
+
+                textarea.style.height = origHeight;
+                textarea.style.overflow = overflow;
+                return height;
+            }
+        }
+        else {
+            return scrollHeight;
+        }
+    });
+    let taHeight = calculateContentHeight(taLineHeight);
+    console.log("taHeight", taHeight);
+    let numberOfLines = Math.ceil(taHeight / taLineHeight) - 3;
+
+    linesChip.innerHTML = numberOfLines.toString().concat(" lines"); 
+}));
